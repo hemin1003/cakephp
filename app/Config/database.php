aa343d1bc2d585d14fabccdce1555abce6c56a05
minbo <hemin_it@163.com>
2017-07-13 19:01:22 +0800

    update

--- app/Config/database.php
+++ app/Config/database.php
@@ -72,7 +72,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'mydb',
+		'database' => 'test',
 		'prefix' => '',
 		'encoding' => 'utf8'
 	);
